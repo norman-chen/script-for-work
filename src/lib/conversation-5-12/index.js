1feb5e9644759b9fdb21d75c5870d965d4a54700
norman-chen <nochen@xogrp.com>
2020-05-18 17:57:59 +0800

    stash

--- src/lib/conversation-5-12/index.js
+++ src/lib/conversation-5-12/index.js
@@ -7,22 +7,17 @@ var rab       = new Rabbit();
 
 const config = {
     xoDsConfig: {
-        // "write-store": {
-        //     "conversation": "postgres://localservices:!q2w3e4r5t6y7@conversations-pg-qa.cfjnafc8bsrz.us-east-1.rds.amazonaws.com:5432/conversation",
-        //     "identity": "postgres://localservices:!q2w3e4r5t6y7@identities-pg-qa.cfjnafc8bsrz.us-east-1.rds.amazonaws.com:5432/identity"
-        //   }
-
-          "write-store": {
-            "conversation": "postgres://localservices:XvkTraVU94enXcud@conversations-pg-prod.cfjnafc8bsrz.us-east-1.rds.amazonaws.com:5432/conversation",
-            "identity": "postgres://localservices:eU89MuEzSALZrAEX@identities-pg-prod.cfjnafc8bsrz.us-east-1.rds.amazonaws.com:5432/identity"
+        "write-store": {
+            "conversation": "postgres://localservices:!q2w3e4r5t6y7@conversations-pg-qa.cfjnafc8bsrz.us-east-1.rds.amazonaws.com:5432/conversation",
+            "identity": "postgres://localservices:!q2w3e4r5t6y7@identities-pg-qa.cfjnafc8bsrz.us-east-1.rds.amazonaws.com:5432/identity"
           }
+
+        //   "write-store": {
+        //     "conversation": "postgres://localservices:XvkTraVU94enXcud@conversations-pg-prod.cfjnafc8bsrz.us-east-1.rds.amazonaws.com:5432/conversation",
+        //     "identity": "postgres://localservices:eU89MuEzSALZrAEX@identities-pg-prod.cfjnafc8bsrz.us-east-1.rds.amazonaws.com:5432/identity"
+        //   }
     },
-    DB: 'storefront',
-    requestInfo :{
-        url   : 'https://prod-sales-api.localsolutions.theknot.com',
-        token : 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJNeUFjY291bnQgQWRtaW4gQXBwbGljYXRpb24iLCJpYXQiOjE0OTk0MTc4MDEsImV4cCI6NDY1NTA5MDkxNywiYXVkIjoiaHR0cDovL3BhcnRuZXJzYWRtaW4udGhla25vdC5jb20iLCJzdWIiOiJ2MSBzZWN1cml0eSIsImNsaWVudElkIjoiNTQ5MTUzM2ItNTI2Ny00NzhmLThlOWItNTNkNzUyYWQ5ZTRhIn0.jF218AKdaS8q3gFa_ixn5KZA0wht9xgzR-MVNdeizqY',
-        apikey: '2c3e706eec474381a58aef482a5cff68'
-    },
+
     // "xoRab": {
     //     "ssl": false,
     //     "user": "local-vx",
@@ -32,12 +27,19 @@ const config = {
     //     "globalExchange": "local-vx"
     // }
 
+    // xoRab: {
+    //     "ssl": false,
+    //     "user": "local-comms",
+    //     "password": "YmFnq2GCLHtk7urJ5eAhKVyb",
+    //     "server": "sprightly-chimpanzee.rmq.cloudamqp.com",
+    //     "vhost": "kxlhkxqc",
+    //     "globalExchange": "local-global"
+    // }
+
     xoRab: {
-        "ssl": false,
-        "user": "local-comms",
-        "password": "YmFnq2GCLHtk7urJ5eAhKVyb",
-        "server": "sprightly-chimpanzee.rmq.cloudamqp.com",
-        "vhost": "kxlhkxqc",
+        "user": "guest",
+        "password": "guest",
+        "server": "rabbitbox",
         "globalExchange": "local-global"
     }
 };
@@ -65,7 +67,7 @@ const config = {
         }
 
         await rab.emit(message, {
-            queueName: 'hapi-segmentio'
+            queueName: 'hapi-segmentio-test-received'
         })
         console.log(`-----i ${i}`);
     }
