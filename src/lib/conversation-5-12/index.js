c01e0b2a925570f4d19d872c265c8d6284fb46c0
birdream <bird.dream.norman@gmail.com>
2020-05-21 17:04:29 +0800

    stash

--- src/lib/conversation-5-12/index.js
+++ src/lib/conversation-5-12/index.js
@@ -4,18 +4,19 @@ const XoDs = require('xo-ds-handler');
 const xoDs = new XoDs()
 var Rabbit         = require('xo-rabbit');
 var rab       = new Rabbit();
+const Promise = require('bluebird')
 
 const config = {
     xoDsConfig: {
-        "write-store": {
-            "conversation": "postgres://localservices:!q2w3e4r5t6y7@conversations-pg-qa.cfjnafc8bsrz.us-east-1.rds.amazonaws.com:5432/conversation",
-            "identity": "postgres://localservices:!q2w3e4r5t6y7@identities-pg-qa.cfjnafc8bsrz.us-east-1.rds.amazonaws.com:5432/identity"
-          }
-
-        //   "write-store": {
-        //     "conversation": "postgres://localservices:XvkTraVU94enXcud@conversations-pg-prod.cfjnafc8bsrz.us-east-1.rds.amazonaws.com:5432/conversation",
-        //     "identity": "postgres://localservices:eU89MuEzSALZrAEX@identities-pg-prod.cfjnafc8bsrz.us-east-1.rds.amazonaws.com:5432/identity"
+        // "write-store": {
+        //     "conversation": "postgres://localservices:!q2w3e4r5t6y7@conversations-pg-qa.cfjnafc8bsrz.us-east-1.rds.amazonaws.com:5432/conversation",
+        //     "identity": "postgres://localservices:!q2w3e4r5t6y7@identities-pg-qa.cfjnafc8bsrz.us-east-1.rds.amazonaws.com:5432/identity"
         //   }
+
+          "write-store": {
+            "conversation": "postgres://localservices:XvkTraVU94enXcud@conversations-pg-prod.cfjnafc8bsrz.us-east-1.rds.amazonaws.com:5432/conversation",
+            "identity": "postgres://localservices:eU89MuEzSALZrAEX@identities-pg-prod.cfjnafc8bsrz.us-east-1.rds.amazonaws.com:5432/identity"
+          }
     },
 
     // "xoRab": {
@@ -27,21 +28,21 @@ const config = {
     //     "globalExchange": "local-vx"
     // }
 
-    // xoRab: {
-    //     "ssl": false,
-    //     "user": "local-comms",
-    //     "password": "YmFnq2GCLHtk7urJ5eAhKVyb",
-    //     "server": "sprightly-chimpanzee.rmq.cloudamqp.com",
-    //     "vhost": "kxlhkxqc",
-    //     "globalExchange": "local-global"
-    // }
-
     xoRab: {
-        "user": "guest",
-        "password": "guest",
-        "server": "rabbitbox",
+        "ssl": false,
+        "user": "local-comms",
+        "password": "YmFnq2GCLHtk7urJ5eAhKVyb",
+        "server": "sprightly-chimpanzee.rmq.cloudamqp.com",
+        "vhost": "kxlhkxqc",
         "globalExchange": "local-global"
     }
+
+    // xoRab: {
+    //     "user": "guest",
+    //     "password": "guest",
+    //     "server": "rabbitbox",
+    //     "globalExchange": "local-global"
+    // }
 };
 
 ;(async () => {
@@ -49,16 +50,17 @@ const config = {
     rab.config(config.xoRab);
 
     const sql = `select * from conversations
-    where data->'timestamp'->>'createdAt' >= '2020-05-12'
-    and data->'timestamp'->>'createdAt' < '2020-05-13'
+    where data->'timestamp'->>'createdAt' >= '2020-05-13'
+    and (data->'timestamp'->>'createdAt')::timestamp < '2020-05-13 11:00'
     `
 
     const convers = await xoDs.pg.execute('conversation', sql)
 
+    let all = []
+    const last = (convers.length-1);
     for (let i = 0; i < convers.length; i++) {
         const { data: conver } = convers[i];
-        // console.log(conver)
-        // continue
+
         const message = {
             event: 'conversation.conversation_created',
             'initial-payload': conver,
@@ -66,10 +68,17 @@ const config = {
             'db-payload': conver
         }
 
-        await rab.emit(message, {
-            queueName: 'hapi-segmentio-test-received'
-        })
-        console.log(`-----i ${i}`);
+        all.push(
+            rab.emit(message, {
+                queueName: 'hapi-segmentio'
+            })
+        )
+
+        if (all.length === 300 || i === last) {
+            await Promise.all(all)
+            all = []
+            console.log(`---------${i}`)
+        }
     }
 
     console.log('--done')
